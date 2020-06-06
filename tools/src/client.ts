89e6cd09e64b82eb2c699cc7be48ad1e42f27f04
Muthu Kumar <muthukumar@thefeathers.in>
2020-06-07 05:19:17 +0530

    [feat] add client

---
+++ tools/src/client.ts
@@ -0,0 +1,37 @@
+import { connect } from "net";
+import { createInterface } from "readline";
+import { resolve } from "path";
+import { EOL } from "os";
+
+import { config as load, proc as loadProc, Proc } from "./loadConfig";
+
+const { proc: procPath, services } = load("config.json");
+
+const name = process.argv[2];
+const app = services.find(each => each.name === name);
+
+if (app) {
+	const masterCWD = process.cwd();
+
+	const procDir = resolve(masterCWD, procPath);
+
+	const proc = loadProc(resolve(procDir, app.name + ".json"));
+
+	const client = connect(Number(proc.port), proc.ip);
+	client.unref();
+
+	const clientrl = createInterface({ input: client });
+	const rl = createInterface({ input: process.stdin });
+
+	client.on("close", () => {
+		console.log("[info] Connection to", proc.name, "lost, exiting...");
+
+		clientrl.close();
+		rl.close();
+	});
+
+	rl.on("line", line => client.write(line + EOL));
+	clientrl.on("line", line => console.log(line));
+} else {
+	console.error("[error] Process not found: " + name);
+}
