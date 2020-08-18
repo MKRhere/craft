de213cfe37908f3df0ddea855aacf834da139201
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-19 02:40:08 +0530

    [feat] Login page

---
+++ craftfactions/client/src/pages/Login.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { css } from "emotion";
+
+import Button from "../components/Button";
+import Input from "../components/Input";
+
+const container = css`
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+	width: 40%;
+	margin: auto;
+	align-items: center;
+	justify-content: center;
+
+	& * {
+		margin: 10px auto;
+		width: 100%;
+		max-width: 400px;
+	}
+
+	@media screen and (max-width: 720px) {
+		width: 80%;
+	}
+`;
+
+const img = css`
+	margin-bottom: 4rem;
+`;
+
+function Login() {
+	const handleSubmit = e => e.preventDefault();
+
+	return (
+		<form className={container}>
+			<img className={img} src="/assets/images/logo.png"></img>
+			<Input
+				type="text"
+				placeholder="Enter username"
+				spellcheck="false"></Input>
+			<Button onClick={handleSubmit}>Login</Button>
+		</form>
+	);
+}
+
+export default Login;
