cc3188c6a430cef01102db94c598be0a1b653917
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-19 02:39:22 +0530

    [feat] add router

--- craftfactions/client/src/App.js
+++ craftfactions/client/src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Router } from "@reach/router";
+
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-	return <div className="App"></div>;
+	const loggedIn = true;
+
+	return (
+		<Router>
+			{!loggedIn ? <Login path="/" /> : <Dashboard path="/" />}
+		</Router>
+	);
 }
 
 export default App;
