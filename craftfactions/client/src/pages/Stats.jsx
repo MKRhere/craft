c2a99c6fe2e953fe88624d5ff7b419ff01b76fd5
rosh <roshinift2lev@gmail.com>
2020-08-20 11:06:06 +0530

    changes in stats

--- craftfactions/client/src/pages/Stats.jsx
+++ craftfactions/client/src/pages/Stats.jsx
@@ -3,9 +3,24 @@ import { css } from "emotion";
 
 import Button from "../components/Button";
 import Input from "../components/Input";
+import Axios from "axios";
 
 function Stats() {
-	return <></>;
+
+	const callAPI = () => 
+	   axios
+	   .get('https://mkr.thefeathers.in/player/5f39b87881505ced8b9b8e92/stats')
+	   .then(res => {
+        const statss = res.stats["minecraft:custom"]
+        
+      })
+  
+	
+	return (
+		<div className="wrapper">
+			{callAPI()}
+		</div>
+	);
 }
 
 export default Stats;
