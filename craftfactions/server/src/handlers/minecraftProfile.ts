5cf4781f85daa708fa8817c39477c975b7f25ab8
Muthu Kumar <muthukumar@thefeathers.in>
2020-08-23 04:23:05 +0530

    [feat] api for skin and cape

---
+++ craftfactions/server/src/handlers/minecraftProfile.ts
@@ -0,0 +1,43 @@
+import type { MojangProfile, MojangProfileTexture } from "../util/minecraft";
+
+import { Router } from "express";
+import { getSkin } from "../util/minecraft";
+import { asyncHandler, createError } from "../util/asyncHandler";
+import { toBase64 } from "../util";
+
+const router = Router();
+
+router.get(
+	"/session/minecraft/profile/:id",
+	asyncHandler(async (req, res) => {
+		let result;
+		try {
+			result = await getSkin({ id: req.params.id });
+		} catch {}
+
+		if (!result) throw createError("User not found", 404);
+
+		const texture: MojangProfileTexture = {
+			timestamp: Date.now(),
+			profileId: req.params.id,
+			profileName: result.name,
+			textures: {
+				SKIN: result.skin ? { url: result.skin } : undefined,
+				CAPE: result.cape ? { url: result.cape } : undefined,
+			},
+		};
+
+		const profile: MojangProfile = {
+			id: req.params.id,
+			name: result.name,
+			properties: [
+				{
+					name: "textures",
+					value: toBase64(JSON.stringify(texture)),
+				},
+			],
+		};
+
+		res.send(profile);
+	}),
+);
--- craftfactions/server/src/util/index.ts
+++ craftfactions/server/src/util/index.ts
@@ -2,3 +2,8 @@ export const id = <T>(x: T) => x;
 
 export const path = <T extends string, U extends {}>(segments: T[], obj: U) =>
 	segments.reduce((o, key) => o && o[key], obj as any);
+
+export const toBase64 = (s: string) => Buffer.from(s).toString("base64");
+
+export const fromBase64 = (s: string) =>
+	Buffer.from(s, "base64").toString("ascii");
