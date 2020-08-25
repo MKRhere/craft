007e42c30dcee6b7b40c73b51c288800af20d04b
rosh <roshinift2lev@gmail.com>
2020-08-25 11:44:17 +0530

    Killed and killed by changes

--- craftfactions/client/src/pages/Stats.jsx
+++ craftfactions/client/src/pages/Stats.jsx
@@ -10,45 +10,55 @@ function Stats() {
 const [statss,setStatss] = useState([]);
 
 const container = css`
-  p {
-	margin: auto;
-	width: 50%;
-	display: flex;
-  }
+  
 `;
 
 const active = css`
-color: #ccc;
+
 `;
 
 const mystyle = css`
 	margin: auto;
 	width: 50%;
-	display: flex;
 	
   `;
 
 const mystyle1 = css`
-	flex: 1;
+	
 `;
 
 const mystyle2 = css`
-	text-align: right;	
+	color: #ccc;
+`;
+
+const mystyle3 = css`
+color: #ccc;
 `;
 	const callAPI = () => 
 	   axios
 	   .get('https://mkr.thefeathers.in/player/5f39b87881505ced8b9b8e92/stats')
 	   .then(res => res.data)
 			.then(res => {
-				console.log(res);
-				const stats = res.stats["minecraft:custom"];
+				
+				const stats = res.stats["minecraft:killed"]
 				console.log(stats);
-				const list =Object.keys(stats).map((key, index) => (
+				const stats1 = res.stats["minecraft:killed_by"]
+				console.log(stats1);
+				
+				//let merged = {...stats, ...stats1};
+				//console.log (merged);
+				const result={};
+
+				Object.keys(stats).forEach(key => result[key] = stats[key]);
+				Object.keys(stats1).forEach(key => result[key] = stats1[key]);
+				console.log(result);
+				const list =Object.keys(result).map((key, index) => (
 					<div className={container}>
-					<p className={`${mystyle} ${index % 2 === 0 ? active : ""}`}>
+					<p className={mystyle}>
 					  <div className={mystyle1}> {key}</div>
-					  <div className={mystyle2}> {stats[key]}</div>
-					
+					  <div className={mystyle2}> you killed {stats[key]} {key}</div>
+					  <div className={mystyle3}>{stats1[key]? key + " killed you " + stats1[key] + " times"
+					  : "You have never been killed by " + key}</div>
 					</p>
 					</div>
 				  ))
