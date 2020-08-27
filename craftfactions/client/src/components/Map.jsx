0b40d83e9c5bb2731434c674f7c07c461a9de685
rosh <roshinift2lev@gmail.com>
2020-08-27 17:35:30 +0530

    adding input fields in Map

--- craftfactions/client/src/components/Map.jsx
+++ craftfactions/client/src/components/Map.jsx
@@ -1,11 +1,25 @@
 import React, { useRef, useEffect } from "react";
+import { css } from "emotion";
+import { useState } from 'react';
 
+const iframecss = css`
+width : 100%;
+height :100%;
+`;
+
+const input = css`
+
+`;
 
 function Map(props){
     const container = useRef(null);
-    const x = props.x;
-    const y = props.y;
-    const z = props.z;
+    const [inputx, setInputx] = useState("");
+    const [inputy, setInputy] = useState("");
+    const [inputz, setInputz] = useState("");
+
+    const x = inputx;
+    const y = inputy;
+    const z = inputz;
 
 	useEffect(() => {
 		if (container.current) {
@@ -15,10 +29,17 @@ function Map(props){
 	}, [container.current]);
 
     return (
-    
-    <iframe frameBorder="0" style={{ width: "100%", height: "100%"}} ref={container}
+    <div>
+    <iframe frameBorder="0" className={iframecss} ref={container}
                 src={`https://map.craft.mkr.pw/#overworld/0/0/${x}/${z}/${y}`}>
             </iframe>
+            <input {...props} value={inputx} className={input} type='text' 
+            onChange={e => setInputx(e.target.value)} />
+            <input {...props} value={inputy} className={input} type='text' 
+            onChange={e => setInputy(e.target.value)} />
+            <input {...props} value={inputz} className={input} type='text' 
+            onChange={e => setInputz(e.target.value)} />
+            </div>
     );
 }
 
