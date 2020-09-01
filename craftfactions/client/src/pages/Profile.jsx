73cbdf87d2a94b80fea2a53f800ac44eb1e01e56
akshan <akshan@gmail.com>
2020-09-01 17:44:47 +0530

    profile page updated

--- craftfactions/client/src/pages/Profile.jsx
+++ craftfactions/client/src/pages/Profile.jsx
@@ -3,7 +3,51 @@ import { css } from "emotion";
 import Button from "../components/Button";
 import SkinView from "../components/SkinView";
 import { navigate } from "@reach/router";
-import List from "../components/List";
+
+const values = css`
+  color: #fff;
+`;
+
+const listprop = css`
+  color: #aaa;
+  margin-top: 20px;
+
+  & p {
+    margin: 0;
+  }
+`;
+
+const link = css`
+  color: #fff;
+`;
+
+function Value(props) {
+  return <span className={values}>{props.children}</span>;
+}
+
+function ProfileDetails(props) {
+  return (
+    <div className={listprop}>
+      <p>
+        Name: <Value>{props.name}</Value>
+      </p>
+      <p>
+        Location:{" "}
+        <Value>
+          <a className={link} href="#">
+            {props.location.join(", ")}
+          </a>
+        </Value>
+      </p>
+      <p>
+        Faction: <Value>{props.faction}</Value>
+      </p>
+      <p>
+        Days on Server: <span className={values}>{props.days}</span>
+      </p>
+    </div>
+  );
+}
 
 const skinDiv = css`
   max-width: 200px;
@@ -12,6 +56,7 @@ const skinDiv = css`
   border: 2px solid white;
   overflow: hidden;
 `;
+
 const mainDiv = css`
   display: flex;
   flex-flow: row wrap;
@@ -19,20 +64,13 @@ const mainDiv = css`
   margin: 10%;
   min-height: 500px;
 `;
+
 const rightDiv = css`
   margin-left: 20%;
   min-width: 400px;
   min-height: 400px;
 `;
 
-const Listvalue = css`
-  color: gray;
-  list-style: none;
-  padding: 0;
-`;
-const values = css`
-  color: white;
-`;
 function Profile() {
   const handleChange = (e) => {
     e.preventDefault();
@@ -48,25 +86,7 @@ function Profile() {
           <Button onClick={handleChange}>Stats</Button>
           <Button>Diplomacy</Button>
         </div>
-        {/* <ul className={`${Listvalue}`}>
-          <li>
-            Name: <span className={`${values}`}>MKRhere</span>
-          </li>
-          <li>
-            Location:{" "}
-            <a href="#" className={`${values}`}>
-              {" "}
-              120,64,1203
-            </a>
-          </li>
-          <li>
-            Faction: <span className={`${values}`}>Fireborn Clan</span>
-          </li>
-          <li>
-            Days on Server: <span className={`${values}`}>300</span>
-          </li>
-        </ul> */}
-        <List
+        <ProfileDetails
           name="MKRhere"
           faction="Fireborn Clan"
           location={[120, 64, 1203]}
