bcaa6ca9728282732cd81462e559563a01935759
Udit Karode <udit.karode@gmail.com>
2022-01-11 10:01:57 +0530

    fix(Init): don't initialise store on client init

--- mods/mkrcraft/src/main/kotlin/pw/mkr/craft/Init.kt
+++ mods/mkrcraft/src/main/kotlin/pw/mkr/craft/Init.kt
@@ -3,14 +3,12 @@ package pw.mkr.craft
 import net.fabricmc.api.ClientModInitializer
 import pw.mkr.craft.binding.BindingBlock
 import pw.mkr.craft.portals.PortalBlock
-import pw.mkr.craft.utils.StoreManager
 
 @Suppress("Unused")
 object Init : ClientModInitializer {
     const val MOD_ID = "mkrcraft"
 
     override fun onInitializeClient() {
-        StoreManager.init()
         BindingBlock.register()
         PortalBlock.register()
     }
