c7a1d3cc4f25b1ecf341ebc1aaf42d3776b0ccc6
Udit Karode <udit.karode@gmail.com>
2022-01-20 19:23:20 +0530

    feat: reduce duplication while registering blocks

--- mods/mkrcraft/src/main/kotlin/pw/mkr/craft/blocks/binding/Binding.kt
+++ mods/mkrcraft/src/main/kotlin/pw/mkr/craft/blocks/binding/Binding.kt
@@ -1,26 +1,17 @@
 package pw.mkr.craft.blocks.binding
 
-import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.minecraft.block.Block
 import net.minecraft.block.BlockState
-import net.minecraft.block.Material
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.entity.LivingEntity
 import net.minecraft.entity.player.PlayerEntity
-import net.minecraft.item.BlockItem
 import net.minecraft.item.ItemGroup
 import net.minecraft.item.ItemStack
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.stat.Stats
 import net.minecraft.text.LiteralText
-import net.minecraft.util.ActionResult
-import net.minecraft.util.Hand
-import net.minecraft.util.Identifier
-import net.minecraft.util.hit.BlockHitResult
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.ChunkPos
-import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 import pw.mkr.craft.models.Binding
 import pw.mkr.craft.models.toModel
@@ -125,15 +116,7 @@ class BindingBlock(settings: Settings) : Block(settings) {
     }
 
     companion object {
-        fun register() {
-            val bindingBlock =
-                BindingBlock(
-                    FabricBlockSettings.of(Material.METAL).strength(4.0f).requiresTool()
-                )
-            val bindingItem = BlockItem(bindingBlock, FabricItemSettings().group(ItemGroup.MISC))
-
-            Registry.register(Registry.BLOCK, Identifier("mkrcraft", "binding_block"), bindingBlock)
-            Registry.register(Registry.ITEM, Identifier("mkrcraft", "binding_block"), bindingItem)
-        }
+		fun register() =
+			Utils.registerBlock(::BindingBlock,"portal_block", 4f, ItemGroup.MISC)
     }
 }
--- mods/mkrcraft/src/main/kotlin/pw/mkr/craft/utils/Utils.kt
+++ mods/mkrcraft/src/main/kotlin/pw/mkr/craft/utils/Utils.kt
@@ -1,12 +1,39 @@
 package pw.mkr.craft.utils
 
 import net.fabricmc.api.EnvType
+import net.fabricmc.fabric.api.`object`.builder.v1.block.FabricBlockSettings
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.loader.api.FabricLoader
+import net.minecraft.block.AbstractBlock
+import net.minecraft.block.Block
+import net.minecraft.block.Material
+import net.minecraft.item.BlockItem
+import net.minecraft.item.ItemGroup
+import net.minecraft.util.Identifier
+import net.minecraft.util.registry.Registry
 import org.apache.logging.log4j.LogManager
+import kotlin.reflect.KFunction1
 
 object Utils {
-    val isServer = FabricLoader.getInstance().environmentType == EnvType.SERVER
-    val logger = LogManager.getLogger()!!
+	val isServer = FabricLoader.getInstance().environmentType == EnvType.SERVER
+	val logger = LogManager.getLogger()!!
 
-    const val DAY_MULTIPLIER = 1  / (20 * 60 * 60 * 24)
+	const val DAY_MULTIPLIER = 1 / (20 * 60 * 60 * 24)
+
+	fun registerBlock(
+		blockConstructor: KFunction1<AbstractBlock.Settings, Block>,
+		blockName: String,
+		strength: Float,
+		group: ItemGroup
+	) {
+		val block =
+			blockConstructor(
+				FabricBlockSettings.of(Material.METAL).strength(strength).requiresTool()
+			)
+
+		val blockItem = BlockItem(block, FabricItemSettings().group(group))
+
+		Registry.register(Registry.BLOCK, Identifier("mkrcraft", blockName), block)
+		Registry.register(Registry.ITEM, Identifier("mkrcraft", blockName), blockItem)
+	}
 }
