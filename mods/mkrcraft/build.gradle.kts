f37a3e0596b70d2c90f0bc01be31a6ef198bf401
Udit Karode <udit.karode@gmail.com>
2022-01-20 18:36:33 +0530

    chore(build.gradle.kts): use tabs

--- mods/mkrcraft/build.gradle.kts
+++ mods/mkrcraft/build.gradle.kts
@@ -1,12 +1,12 @@
 plugins {
-    id("fabric-loom")
-    val kotlinVersion: String by System.getProperties()
-    kotlin("jvm").version(kotlinVersion)
+	id("fabric-loom")
+	val kotlinVersion: String by System.getProperties()
+	kotlin("jvm").version(kotlinVersion)
 }
 
 base {
-    val archivesBaseName: String by project
-    archivesName.set(archivesBaseName)
+	val archivesBaseName: String by project
+	archivesName.set(archivesBaseName)
 }
 
 val modVersion: String by project
@@ -19,51 +19,51 @@ minecraft {}
 repositories {}
 
 sourceSets.main {
-    java.srcDirs("src/main/kotlin/pw/mkr/craft/mixins")
+	java.srcDirs("src/main/kotlin/pw/mkr/craft/mixins")
 }
 
 dependencies {
-    val minecraftVersion: String by project
-    minecraft("com.mojang:minecraft:$minecraftVersion")
-    val yarnMappings: String by project
-    mappings("net.fabricmc:yarn:$yarnMappings:v2")
-    val loaderVersion: String by project
-    modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
-    val fabricVersion: String by project
-    modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
-    val fabricKotlinVersion: String by project
-    modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
+	val minecraftVersion: String by project
+	minecraft("com.mojang:minecraft:$minecraftVersion")
+	val yarnMappings: String by project
+	mappings("net.fabricmc:yarn:$yarnMappings:v2")
+	val loaderVersion: String by project
+	modImplementation("net.fabricmc:fabric-loader:$loaderVersion")
+	val fabricVersion: String by project
+	modImplementation("net.fabricmc.fabric-api:fabric-api:$fabricVersion")
+	val fabricKotlinVersion: String by project
+	modImplementation("net.fabricmc:fabric-language-kotlin:$fabricKotlinVersion")
 
-    implementation("com.google.code.gson:gson:2.8.9")
+	implementation("com.google.code.gson:gson:2.8.9")
 }
 
 tasks {
-    val javaVersion = JavaVersion.VERSION_17
+	val javaVersion = JavaVersion.VERSION_17
 
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-        options.release.set(javaVersion.toString().toInt())
-    }
+	withType<JavaCompile> {
+		options.encoding = "UTF-8"
+		sourceCompatibility = javaVersion.toString()
+		targetCompatibility = javaVersion.toString()
+		options.release.set(javaVersion.toString().toInt())
+	}
 
-    withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions { jvmTarget = javaVersion.toString() }
-        sourceCompatibility = javaVersion.toString()
-        targetCompatibility = javaVersion.toString()
-    }
+	withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+		kotlinOptions { jvmTarget = javaVersion.toString() }
+		sourceCompatibility = javaVersion.toString()
+		targetCompatibility = javaVersion.toString()
+	}
 
-    jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
+	jar { from("LICENSE") { rename { "${it}_${base.archivesName}" } } }
 
-    processResources {
-        inputs.property("version", project.version)
-        filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
-    }
+	processResources {
+		inputs.property("version", project.version)
+		filesMatching("fabric.mod.json") { expand(mutableMapOf("version" to project.version)) }
+	}
 
-    java {
-        toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
-        sourceCompatibility = javaVersion
-        targetCompatibility = javaVersion
-        withSourcesJar()
-    }
+	java {
+		toolchain { languageVersion.set(JavaLanguageVersion.of(javaVersion.toString())) }
+		sourceCompatibility = javaVersion
+		targetCompatibility = javaVersion
+		withSourcesJar()
+	}
 }
