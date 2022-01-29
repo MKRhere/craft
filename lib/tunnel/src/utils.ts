976e46a0a903aa7b9599fd39bb6d7c567b4b1a17
Muthu Kumar <muthukumar@thefeathers.in>
2022-01-29 13:39:21 +0530

    fix(tunnel): use copy, writeAll from std
    Signed-off-by: Muthu Kumar <muthukumar@thefeathers.in>

--- lib/tunnel/src/utils.ts
+++ lib/tunnel/src/utils.ts
@@ -1,18 +1,10 @@
-type AnyF = (...args: any[]) => void;
-
 export * from "./deferred.ts";
+
+export { writeAll, copy } from "https://deno.land/std@0.123.0/streams/conversion.ts";
 
 export const sleep = (t: number) => new Promise(r => setTimeout(r, t));
 
 export const noop = () => void 0;
-
-export const tryElse = <F extends AnyF, E extends AnyF>(f: F, c: E) => {
-	try {
-		f();
-	} catch {
-		c();
-	}
-};
 
 export const assert = (ok: boolean) => {
 	if (!ok) {
@@ -31,31 +23,6 @@ export const readAll = async (r: Deno.Reader, buf: Uint8Array) => {
 		bytesRead += result;
 	}
 	return bytesRead;
-};
-
-export const writeAll = async (w: Deno.Writer, buf: Uint8Array) => {
-	let bytesWritten = 0;
-	while (bytesWritten < buf.byteLength) {
-		bytesWritten += await w.write(buf.subarray(bytesWritten));
-	}
-	return bytesWritten;
-};
-
-export const copy = async (r: Deno.Reader, w: Deno.Writer) => {
-	const buf = new Uint8Array(65535);
-	while (true) {
-		const result = await r.read(buf);
-		if (result === null) {
-			return null;
-		}
-		await w.write(buf.subarray(0, result));
-	}
-};
-
-export const ends = async (r: Deno.Reader) => {
-	let buf = new Uint8Array(65535);
-	while ((await r.read(buf)) !== null) {}
-	return true;
 };
 
 export const close = (...conns: Deno.Closer[]) => {
